5d16efe12c18f117fc359191e7f01c145876becc
Wu <119737938+aloverwu@users.noreply.github.com>
2023-03-06 14:20:06 +0800

    Update LCD.ino

--- LCD.ino
+++ LCD.ino
@@ -6,7 +6,6 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 void setup()
 {
   lcd.init();                      // initialize the lcd 
-  lcd.init();
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(0,0);
